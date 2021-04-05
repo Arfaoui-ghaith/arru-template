cd2bc38e0951fc098f3b6c179759a5f894fc3395
Arfaoui-ghaith <ghaith.arfaoui34@gmail.com>
2021-04-05 03:51:25 +0200

    push quartiers

---
+++ src/identification/componet/MapFormAdd.js
@@ -0,0 +1,207 @@
+import React from 'react'
+import Select from 'react-select'
+import makeAnimated from 'react-select/animated'
+import { Container, Card, Button, Modal } from 'react-bootstrap';
+import { MapContainer , TileLayer, FeatureGroup } from "react-leaflet";
+import { EditControl } from "react-leaflet-draw"
+import axios from 'axios'
+
+export default function MapFormAdd() {
+    const animatedComponents = makeAnimated();
+    
+    const [show, setShow] = React.useState(false);
+    const [projet, setProjet] = React.useState(null);
+    const [nom, setNom] = React.useState("");
+
+    const handleProjetsChange = (e) => {
+        console.log(e.value)
+		setProjet(e.value);
+	}
+
+    const [projets, setProjets] = React.useState([]);
+
+    const [mapLayers, setMapLayers] = React.useState([]);
+
+    const mapRef = React.useRef();
+
+    const fetchProjets = async () => {
+		try {
+			const url ='http://localhost:4000/api/v1/projets/';
+			const res = await axios({
+				headers: {'Authorization': `Bearer ${localStorage.getItem('tokenARRU')}`},
+			  	method: 'get',
+			  	url,
+			});
+	
+			console.log(res.data.projets);
+
+			if (res.status === 200) {
+				let projets_options = [];
+				for(const projet of res.data.projets){
+					let obj = { value: projet.id, label: projet.nom }
+					projets_options.push(obj);
+				}
+				setProjets(projets_options);
+			}
+
+			} catch (err) {
+				console.log(err);
+			}
+	}
+
+    const addQuartier = async () => {
+		try {
+			const url =`http://localhost:4000/api/v1/quartiers/${projet}`;
+			const res = await axios({
+				headers: {'Authorization': `Bearer ${localStorage.getItem('tokenARRU')}`},
+			  	method: 'post',
+			  	url,
+                data:{ quartiers: mapLayers }
+			});
+
+			if (res.status === 201) {
+				console.log(res.data);
+                window.location.replace('/Quartiers');
+			}
+
+			} catch (err) {
+				console.log(err);
+			}
+	}
+
+    React.useEffect(() => {
+        fetchProjets()
+    },[]);
+
+    const _onCreate = (e) => {
+        console.log(e);
+    
+        const { layerType, layer } = e;
+        if (layerType === "polygon") {
+            
+          const { _leaflet_id } = layer;
+          
+          mapLayers.push({ id: _leaflet_id, nom: "", center: layer.getCenter(), latlngs: layer.getLatLngs()[0] });
+        }
+        setShow(true);
+        console.log(mapLayers);
+    };
+    
+    const _onEdited = (e) => {
+        //console.log(e);
+    const {
+        layers: { _layers },
+    } = e;
+    
+    for(let i=0; i<mapLayers.length; i++){
+        for(const layer of Object.values(_layers)){
+            
+            if(mapLayers[i].id === layer._leaflet_id){
+                mapLayers.splice(i,1,{ id: layer._leaflet_id, nom: mapLayers[i].nom, center: layer.getCenter(), latlngs: layer.getLatLngs()[0] });
+            }
+        }
+    }
+
+        console.log(mapLayers);
+    };
+    
+    const _onDeleted = (e) => {
+        //console.log(e);
+    const {
+        layers: { _layers },
+    } = e;
+    
+        
+    console.log(Object.values(_layers));
+
+        
+    for(let i=0; i<mapLayers.length; i++){
+        for(const layer of Object.values(_layers)){
+            
+            if(mapLayers[i].id === layer._leaflet_id){
+                mapLayers.splice(i,1);
+            }
+        }
+    }
+        console.log("delete",mapLayers);
+    };
+
+    return (
+        <React.Fragment>
+        <Container>
+                { projet ?
+                    <Card>
+                        <Card.Header>
+            
+                        </Card.Header>
+                        <Card.Body>
+                            <MapContainer  center={[34.886917, 9.537499]} zoom={7} ref={mapRef}>
+                            <TileLayer
+                                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            />
+                            
+                            <FeatureGroup>
+                                <EditControl
+                                position='topright'
+                                onCreated={_onCreate}
+                                onEdited={_onEdited}
+                                onDeleted={_onDeleted}
+                                click={(e) => console.log(e)}
+                                draw={{
+                                    rectangle: false,
+                                    circle: false,
+                                    circlemarker: false,
+                                    marker: false,
+                                    polyline: false
+                                }}
+                                />
+                                
+                            </FeatureGroup>
+                                
+                            </MapContainer >
+                        </Card.Body>
+                    </Card> : ""
+                }
+                    <Card>
+                        <Card.Header>
+                        <Select className="py-2"
+                            placeholder="Select Projet ..."
+							closeMenuOnSelect={false}
+							components={animatedComponents}
+							options={projets}
+                            onChange={handleProjetsChange}
+						/>	
+                        </Card.Header>
+                        <Card.Body>	  
+                            
+                            <Button variant="primary" size="lg" onClick={addQuartier} block>
+                                Enregistrer
+                            </Button> 
+                        </Card.Body>
+                    </Card>
+
+        </Container>
+
+        <Modal show={show} onHide={() => setShow(false)}>
+            <Modal.Header closeButton>
+            <Modal.Title>Nom quartier</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+            <div className="row">
+                <label className="col-form-label col-sm-1 text-sm-left">nom: </label>
+                <div className="col-sm-11">
+                    <input type="text" className="form-control" onChange={(e) => setNom(e.target.value)} />
+                </div>
+            </div>
+            </Modal.Body>
+            <Modal.Footer>
+           
+            <Button variant="primary" onClick={() => { mapLayers[mapLayers.length - 1].nom = nom; setNom(""); setShow(false); }}>
+                Save
+            </Button>
+            </Modal.Footer>
+        </Modal>
+        </React.Fragment>
+    )
+}
--- src/identification/pages/Quartiers.js
+++ src/identification/pages/Quartiers.js
@@ -1,38 +1,99 @@
 import React from 'react'
 import Map from '../componet/Map'
+import Select from 'react-select'
+import makeAnimated from 'react-select/animated'
+import axios from 'axios'
+
+import FeatherIcon from 'feather-icons-react';
+import MapFormAdd from './../componet/MapFormAdd'
+import { Container, Row, Col, Modal, Card, Button } from 'react-bootstrap';
 
 export default function Quartiers() {
+    const animatedComponents = makeAnimated();
+    const [show, setShow] = React.useState(false);
+    
+
     return (
-        <main class="content">
-        <div class="container-fluid p-0">
+        <main className="content">
+        <div className="container-fluid p-0">
 
-            <h1 class="h3 mb-3">Gestion des quartiers</h1>
+            <h1 className="h3 mb-3">Gestion des quartiers</h1>
 
-            <div class="row">
-                <div class="col-12 col-lg-12" >
-                    <div class="card">
-                        <div class="card-header">
-                            <h5 class="card-title">Default Map</h5>
-                            <h6 class="card-subtitle text-muted">Displays the default road map view.</h6>
+            <div className="row">
+                <div className="col-12 col-lg-12" >
+                    <div className="card">
+                        <div className="card-header">
+                            <h5 className="card-title">Default Map</h5>
+                            <h6 className="card-subtitle text-muted">Displays the default road map view.</h6>
                         </div>
                     </div>
-                    <div class="content" id="default_map" style={{"height": "300px", "position": "relative", "overflow": "hidden", "margin":"0px"}}>
-                            <div style={{"height": "100%", "width": "100%", "position": "absolute", "top": "0px", "left": "0px", "background-color": "rgb(229, 227, 223)"}}>
-                                <div class="gm-err-container">
-                                    <div class="gm-err-content">
-                                        <div class="gm-err-icon">
-                                        <Map class="px-3 card-body" />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        
-                </div>
-                
+                    <Container>
+                        <Row>
+                            <Col xs={12} md={3}>
+                            <Card>
+                                <Card.Header>
+                                    <Button variant="primary" size="lg" block onClick={() => setShow(true)}>
+                                        <FeatherIcon icon="map" /><FeatherIcon icon="plus" />
+                                    </Button>
+                                </Card.Header>
+                                <Card.Body>
+                                
+                                <Select className="py-2"
+                                    placeholder="Gouvernorat ..."
+                                    closeMenuOnSelect={false}
+                                    components={animatedComponents}
+                                    options={[]}
+                                />
+                                <Select className="py-2"
+                                    placeholder="Commune ..."
+                                    closeMenuOnSelect={false}
+                                    components={animatedComponents}
+                                    options={[]}
+                                />
+                                <Select className="py-2"
+                                    placeholder="Municipalite ..."
+                                    closeMenuOnSelect={false}
+                                    components={animatedComponents}
+                                    options={[]}
+                                />	
+                                	   
+                                </Card.Body>
+                            </Card>
+                            </Col>
+                            <Col xs={12} md={9} >
+                            <Card>
+                                <Card.Header>
+                    
+                                </Card.Header>
+                                <Card.Body>
+                                    <Map />
+                                </Card.Body>
+                            </Card>
+                            </Col>
+                        </Row>
+                   </Container>         
+                </div> 
             </div>
 
         </div>
+
+        <Modal
+        show={show}
+        size="lg"
+        onHide={() => setShow(false)}
+        dialogClassName="modal-100w"
+        aria-labelledby="example-custom-modal-styling-title"
+        >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-custom-modal-styling-title">
+            Ajouter Quartier
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="show-grid">
+         <MapFormAdd />
+        </Modal.Body>
+      </Modal>
+
     </main>
     )
 }
